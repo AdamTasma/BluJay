f698a293b2c227658b61d064edcd64b4e09523c0
Adam Tasma <adamtasma@gmail.com>
2018-04-03 19:57:53 -0400

    added currentCar as a sort of global variable to be passed around

--- BluJay/Program.cs
+++ BluJay/Program.cs
@@ -8,45 +8,50 @@ namespace BluJay
 {
     class Program
     {
-        
-
         static void Main()
         {
+            string currentCar = "no car";
             Semi s = new Semi();
             SUV suv = new SUV();
+
             Console.WriteLine("Welcome to a traffic accident waiting to happen!");
-            ChooseCar();
-            ShowMenu(s, suv);
+            ChooseCar(currentCar);
+            ShowMenu(s, suv, currentCar);
             
         }
 
-        static void ChooseCar()
+        public static string ChooseCar(string currentCar)
         {
-            Console.WriteLine("Which car would you like to drive? enter 1 for an 18 wheeler semi, enter 2 for an SUV");
+            Console.WriteLine("Which car would you like to drive?\n" + 
+                              "1) for an 18 wheeler semi\n" + 
+                              "2) for an SUV");
             if (int.TryParse(Console.ReadLine(), out int carChoice))
             {
                 if (carChoice == 1)
                 {
-                    Semi s = new Semi();
+                    currentCar = "semi";
+                    return currentCar;
                 }
                 else if (carChoice == 2)
                 {
-                    //create suv object
+                    currentCar = "suv";
+                    return currentCar;
                 }
             }
             else
             {
                 Console.WriteLine("Oh no! Maybe roadrage has clouded your ability to answer correctly. Please make sure to only enter a numeric 1 or 2.");
-                ChooseCar();
+                ChooseCar(currentCar);
             }
-            Console.ReadLine();
+            return "-1";
         }
 
-        public static void ShowMenu(Semi s, SUV suv)
+        public static void ShowMenu(Semi s, SUV suv, string currentCar)
         {
             TrafficLight tl = new TrafficLight();
-            Console.WriteLine("Up ahead a traffic light is " + tl.trafficLightColor);
-            Console.WriteLine("What would you like to do next? please enter the corresponding number.");
+            Console.WriteLine("\nUp ahead a traffic light is " + tl.trafficLightColor + ".");
+            System.Threading.Thread.Sleep(100);
+            Console.WriteLine("\nWhat would you like to do next? please enter the corresponding number.");
             Console.WriteLine("1) Continue to drive your vehicle\n" +
                               "2) Wait for the traffic light to change colors\n" +
                               "3) Change vehicles\n" +
@@ -65,11 +70,12 @@ namespace BluJay
                         Console.WriteLine();
                         break;
                 }
+                ShowMenu(s, suv, currentCar);
             }
             else
             {
                 Console.WriteLine("Please enter a numeric number that corresponds with your options.");
-                ShowMenu(s, suv);
+                ShowMenu(s, suv, currentCar);
             }
         }
     }
--- BluJay/Semi.cs
+++ BluJay/Semi.cs
@@ -13,12 +13,21 @@ namespace BluJay
 
         public Semi()
         {
-            direction = SemiDirection();
+
         }
+
+        //public Semi()
+        //{
+        //    direction = SemiDirection();
+        //}
 
         public int SemiDirection()
         {
-            Console.WriteLine("Which direction would you like to go?\nEnter 1 for moving foward \nEnter 2 to turn left\n Enter 3 to turn right\nEnter 4 to jackknife to a stop");
+            Console.WriteLine("\nWhich direction would you like to go?\n" +   
+                              "1) Move foward \n" +
+                              "2) Turn left\n" +
+                              "3) Turn right\n" +
+                              "4) Jack knife to a stop");
             if (int.TryParse(Console.ReadLine(), out int userInput))
             {
                 direction = userInput;
@@ -28,63 +37,64 @@ namespace BluJay
                     case 1: //move forward
                         if (previousDirection != 1)
                         {
-                            Console.WriteLine("You move forward at an accelerating pace.");
+                            Console.WriteLine("\nYou move forward at an accelerating pace.");
                             previousDirection = 1;
                             return direction;
                         }
                         else
                         {
-                            Console.WriteLine("Error! Experiencing some highway hypnosis friend? You are already moving forward. Please try a different direction.");
+                            Console.WriteLine("\nError! Experiencing some highway hypnosis friend? You are already moving forward. Please try a different direction.");
                             SemiDirection();
                             break;
                         }
                     case 2: //turn left
                         if (previousDirection != 4)
                         {
-                            Console.WriteLine("You turn to the left.");
+                            Console.WriteLine("\nYou turn to the left.");
                             previousDirection = 2;
                             return direction;
                         }
                         else
                         {
-                            Console.WriteLine("You can't turn yet, your trailer is still jack knifed. Try moving forward first.");
+                            Console.WriteLine("\nYou can't turn yet, your trailer is still jack knifed. Try moving forward first.");
                             SemiDirection();
                             break;
                         }
                     case 3: //turn right
                         if (previousDirection != 4)
                         {
-                            Console.WriteLine("You turn to the right.");
+                            Console.WriteLine("\nYou turn to the right.");
                             previousDirection = 3;
                             return direction;
                         }
                         else
                         {
-                            Console.WriteLine("You can't turn yet, your trailer is still jack knifed. Try moving forward first.");
+                            Console.WriteLine("\nYou can't turn yet, your trailer is still jack knifed. Try moving forward first.");
                             SemiDirection();
                             break;
                         }
                     case 4: //jack knife to a stop
                         if (previousDirection != 4)
                         {
-                            Console.WriteLine("You hit the brakes so quickly that your trailer careens slightly to the side, jack knifing to a stop.");
+                            Console.WriteLine("\nYou hit the brakes so quickly that your trailer careens slightly to the side, jack knifing to a stop.");
                             previousDirection = 4;
                             return direction;
                         }
                         else
                         {
-                            Console.WriteLine("You might still be experiencing vertigo, you are already stopped! Try moving forward to straighten the semi out.");
+                            Console.WriteLine("\nYou might still be experiencing vertigo from braking so hard, you are already stopped! Try moving forward to straighten the semi out.");
+                            SemiDirection();
                             break;
                         }
                     default:
-                        Console.WriteLine("please make sure toenter a numeric between 1-4.");
+                        Console.WriteLine("\nplease make sure to enter a numeric between 1-4.");
                         SemiDirection();
                         break;
                 }
             }
             else
             {
-                Console.WriteLine("Please only enter a numeric keystroke and hit enter.");
+                Console.WriteLine("\nPlease only enter a numeric keystroke and hit enter.");
                 SemiDirection();
                 return direction;
             }
