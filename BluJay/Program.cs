b644267c984675f5ff6e790bd435406144c07301
Adam Tasma <adamtasma@gmail.com>
2018-04-03 17:11:12 -0400

    finished user directions for semi

---
+++ BluJay/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluJay
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to a traffic accident waiting to happen!");
+            ChooseCar();
+        }
+        static void ChooseCar()
+        {
+            Console.WriteLine("Which car would you like to drive? enter 1 for an 18 wheeler semi, enter 2 for an SUV");
+            if (int.TryParse(Console.ReadLine(), out int CarChoice))
+            {
+                if (CarChoice == 1)
+                {
+                    Semi s = new Semi();
+                }
+                else if (CarChoice == 2)
+                {
+                    //create suv object
+                }
+            }
+            else
+            {
+                Console.WriteLine("Oh no! Maybe roadrage has clouded your ability to answer correctly. Please make sure to only enter a numeric 1 or 2.");
+                ChooseCar();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BluJay/Semi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluJay
+{
+    class Semi
+    {
+        int direction;
+
+        public Semi()
+        {
+            direction = UserDirection();
+        }
+
+        public int UserDirection()
+        {
+            Console.WriteLine("Which direction would you like to go?\nEnter 1 for moving foward \nEnter 2 to turn left\n Enter 3 to turn right\nEnter 4 to jackknife to a stop");
+            if (int.TryParse(Console.ReadLine(), out int userInput))
+            {
+                direction = userInput;
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine("You move forward at an accelerating pace.");                       
+                        return direction;
+                    case 2:
+                        Console.WriteLine("You turn to the left.");
+                        return direction;
+                    case 3:
+                        Console.WriteLine("You turn to the right.");
+                        return direction;
+                    case 4:
+                        Console.WriteLine("You hit the brakes so quickly that your trailer careens slightly to the side, jack knifing to a stop.");
+                        return direction;
+                    default:
+                        Console.WriteLine("please enter a numeric between 1-4.");
+                        UserDirection();
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please only enter a numeric keystroke and hit enter.");
+                UserDirection();
+                return direction;
+            }
+            return -1;
+        }
+    }
+
+
+}
